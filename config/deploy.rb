48f19a2217967f2ac7e16738987b33358b433e47
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-06-09 17:13:43 +0300

    fix restart string

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,7 @@ set :keep_releases, 3
 namespace :deploy do
   task :restart do
     on "deployer@192.168.137.75" do
-      # execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -9 cat #{fetch(:unicorn_pid)}; else cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D; fi"
-      execute :kill, '-s USR2', capture(:cat, fetch(:unicorn_pid))
+      execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D; fi"
     end
   end
 
