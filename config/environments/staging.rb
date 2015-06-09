a8cbd2499a837e02c56c53321f9807a045453f72
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-06-09 15:41:11 +0300

    comment nginx config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -22,7 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
