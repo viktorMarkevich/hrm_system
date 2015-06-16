31c0382c8556294929054e98d770774fa7080c0a
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-06-16 12:43:23 +0300

    remove spec_helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
-require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 require 'factory_girl'
