83d01b1874af6fade16b7da4e366fdd5ec1e28c7
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-06-24 10:58:09 +0300

    finish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
-  def active_class(link_path)
-    current_page?(link_path) ? 'active current' : ''
+  def active_class(controller, link_path = nil)
+    if link_path
+      current_page?(link_path) ? 'active current' : ''
+    else
+      params[:controller] == controller && params[:action] != 'new' ? 'active current' : ''
+    end
   end
 
 end
