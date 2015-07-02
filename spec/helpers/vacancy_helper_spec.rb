cde3d93945364d2ff9fffe0420607b369521c432
Dmitriy <bj911@ukr.net>
2015-07-02 14:11:59 +0300

    refactor vacancy

--- spec/helpers/vacancy_helper_spec.rb
+++ spec/helpers/vacancy_helper_spec.rb
@@ -5,15 +5,11 @@ RSpec.describe VacancyHelper, type: :helper do
   let(:vacancy) { create :vacancy }
 
   context 'methods helper' do
-    it 'method get_author_and_date(vacancy)' do
-      # user = create(:user, id: 1)
-      # vacancy = create(:vacancy, user_id: user.id)
-      # expect(helper.full_name_for(user)).to eq(full_name_for(user))
+    it 'method get_author_and_date' do
       expect(helper.get_author_and_date(vacancy)).to eq(get_author_and_date(vacancy))
     end
 
     it 'method get salary' do
-      # vacancy = create(:vacancy)
       expect(helper.get_salary(vacancy)).to eq(helper.get_salary(vacancy))
     end
   end
