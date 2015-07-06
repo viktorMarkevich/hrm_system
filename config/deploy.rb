114c39208272e6c92f08b27651e2166c0cebbf0c
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-07-06 10:50:40 +0300

    cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,5 +59,4 @@ namespace :deploy do
     run "cd #{current_path} && bundle exec rake db:reset RAILS_ENV=#{rails_env}"
   end
 end
-after "deploy:restart", "deploy:cleanup"
-after 'rake:vacancies'
+after "deploy:restart", "deploy:cleanup", 'rake:vacancies'
