6105628bfe0f26757f400af482d654f8cbba1675
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-07-08 20:08:05 +0300

    refactor, second step

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -3,14 +3,20 @@ class ArchivesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @stickers = Sticker.only_deleted.includes(:owner, :performer).order('created_at desc').page(params[:page]).per(8)
+    @objects = klass.includes(:owner, :performer).order('created_at desc').page(params[:page]).per(8)
   end
 
   def destroy
-    @sticker = Sticker.only_deleted.find(params[:id])
+    @sticker = klass.find(params[:id])
     if @sticker.restore
       flash[:notice] = 'Стикер был успешно восстановлен.'
       redirect_to archives_stickers_path
     end
   end
+
+  private
+
+    def klass
+      params[:object_name].classify.constantize.only_deleted
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
 
   root 'stickers#index'
 
-  resources :stickers, except: [:show] do
-    get 'archives', to: 'archives#index', on: :collection
-    delete 'archives', to: 'archives#destroy', on: :member
-  end
+  resources :stickers, except: [:show]
+
+  get 'archives/:object_name', to: 'archives#index', as: :archives
+  delete 'archives/:object_name/:id', to: 'archives#destroy', as: :restore_object
 
   resources :users, only: [:update, :edit, :show, :index]
 
