b7a38d9d40827168abe85b69f4c81bf716cc92c9
roma67 <romchik.liskov@gmail.com>
2015-07-09 15:57:20 +0300

    fix create user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -6,16 +6,15 @@ ActiveAdmin.register User do
   actions :all
 
   collection_action :send_invitation, method: :post do
-    @user = User.invite!(permitted_params[:user])
-    @user.associate_with_region(params[:region])
-
-    if @user.valid? && @user.errors.empty? && !User.find_by(email: @user.email)
-      @user.save
-      flash[:notice] = 'User has been successfully invited.'
-      redirect_to admin_users_path
-    else
+    if User.find_by(email: permitted_params[:user][:email])
       flash[:error] = 'Пользователь с таким email уже существует!'
       redirect_to new_admin_user_path
+    else
+      @user = User.invite!(permitted_params[:user])
+      @user.associate_with_region(params[:region])
+
+      flash[:notice] = 'User has been successfully invited.'
+      redirect_to admin_users_path
     end
   end
 
