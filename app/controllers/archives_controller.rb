1343cd81ed50824e80c44db1fd8304dc041e300f
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-07-09 15:12:19 +0300

    add КОСТЫЛЬ

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,14 +2,14 @@ class ArchivesController < ApplicationController
 
   before_filter :authenticate_user!
 
-  def index
+  def index  #:owner, :performer  - 'проблема'
     @objects = klass.includes(:owner, :performer).order('created_at desc').page(params[:page]).per(8)
   end
 
   def destroy
     @object = klass.with_deleted.find(params[:id])
     if @object.restore
-      flash[:notice] = 'Стикер был успешно восстановлен.'
+      flash[:notice] = 'Объект был успешно восстановлен.'
       redirect_to archives_path(object_name: @object.class.to_s.downcase.pluralize)
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,10 @@ module ApplicationHelper
   def active_class(controller, link_path = nil)
     if link_path
       current_page?(link_path) ? 'active current' : ''
-    else
-      params[:controller] == controller && params[:action] != 'new' ? 'active current' : ''
-    end
+    elsif params[:controller] == controller && params[:action] != 'new' ||
+          params[:object_name] && params[:object_name] == controller
+      'active current'
+    end || ''
   end
 
   def display_field_value_for(field)
