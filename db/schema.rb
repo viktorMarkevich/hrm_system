63e4f68fb5c6cfa92dbcbd505881a3227eeb2918
roma67 <romchik.liskov@gmail.com>
2015-07-20 10:36:17 +0300

    change

--- db/schema.rb
+++ db/schema.rb
@@ -126,7 +126,6 @@ ActiveRecord::Schema.define(version: 20150713122515) do
     t.integer  "performer_id"
     t.datetime "deleted_at"
     t.string   "status"
-    t.string   "progress"
   end
 
   add_index "stickers", ["deleted_at"], name: "index_stickers_on_deleted_at", using: :btree
