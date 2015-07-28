2dbbc8a670be1fa11c63528a7d59206c973a433c
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-07-28 11:51:26 +0300

    fix companies test

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -55,7 +55,6 @@ RSpec.describe CompaniesController, type: :controller do
       end
 
       it %q{ doesn't create record without region_id } do
-        pending
         expect{ post :create, company_attrs }.to change(Company, :count).by(0)
       end
 
