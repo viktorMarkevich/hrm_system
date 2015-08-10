b99d7a34768e38d6607dd5b53d9f7debb5250e1d
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-08-10 17:04:09 +0300

    make create event modal only

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,34 +1,23 @@
 class EventsController < ApplicationController
+
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
   def index
     @events = Event.order(starts_at: :asc)
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
   end
 
-  # GET /events/new
-  def new
-    @event = Event.new
-  end
-
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Событие успешно создано.' }
+        format.html { redirect_to @events, notice: 'Событие успешно создано.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
@@ -37,8 +26,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -51,8 +38,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
@@ -62,12 +47,12 @@ class EventsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_event
     @event = Event.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+
   def event_params
     params.require(:event).permit(:name, :starts_at, :description)
   end
