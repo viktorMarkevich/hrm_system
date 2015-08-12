1fbc9678f0bf07cc92370283ad53bb078d114382
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-08-12 17:07:31 +0300

    change bg color

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -8,7 +8,7 @@ class Sticker < ActiveRecord::Base
   STATUS = %w(Назначен Отложен Закрыт)
   BG_COLOR = %w(#FF3300 #FF66FF #6666FF #00CCFF #00CC66 #009900
                 #99CC00 #FFFF66 #FF9933 #FF9999 #CC6699 #CCCC00
-                #FF3333 #0033CC #339966 #9900CC #D63385 #CC66FF)
+                #FF3333 #3399FF #339966 #9900CC #D63385 #CC66FF)
 
 
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
