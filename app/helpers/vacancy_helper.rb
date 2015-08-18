317553b4741b84bf6326094a3f1b5c2f057952f1
roma67 <romchik.liskov@gmail.com>
2015-08-18 16:46:01 +0300

    temp commit

--- app/helpers/vacancy_helper.rb
+++ app/helpers/vacancy_helper.rb
@@ -4,4 +4,18 @@ module VacancyHelper
     vacancy.salary_format != "По договоренности" ? "#{vacancy.salary} #{vacancy.salary_format}" : "По договоренности"
   end
 
+  # def select_vacancy_status
+  #   vacancies = Vacancy::STATUSES
+  #   options = []
+  #   vacancies.each do |vs|
+  #     options << [
+  #         {
+  #             'Не задействована' =>  {class: 'label label-info'},
+  #             vs[2] =>  {class: 'label label-danger'}
+  #         }
+  #     ]
+  #   end
+  #   options
+  # end
+
 end
