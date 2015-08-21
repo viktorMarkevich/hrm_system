1e538a70c8a3a573de9cce9ed7f6b9644e5aa687
viktorMarkevich <viktor.markevich@faceit.com.ua>
2015-08-21 11:16:16 +0300

    bundle

--- db/schema.rb
+++ db/schema.rb
@@ -87,10 +87,10 @@ ActiveRecord::Schema.define(version: 20150811112934) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "name",        default: "Интервью"
+    t.string   "name"
     t.datetime "starts_at"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.text     "description"
     t.integer  "user_id"
   end
