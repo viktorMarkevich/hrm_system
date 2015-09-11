5ecf5ecc69e0b79fe85fc771dcfb2163bd974ad2
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-09-11 14:48:29 +0300

    sdfsdf

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 20150911073144) do
     t.string   "google_plus"
     t.string   "home_page"
     t.string   "skype"
-    t.integer  "owner_id"
     t.integer  "user_id"
     t.integer  "company_id"
   end
