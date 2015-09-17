90d318457b72a18ddd57203f960e492ecd661e32
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-09-17 16:25:43 +0300

    fix edit form

--- app/models/staff_relation.rb
+++ app/models/staff_relation.rb
@@ -5,13 +5,13 @@ class StaffRelation < ActiveRecord::Base
 
   STATUSES = %w(Нейтральный Найденные Отобранные Собеседование Утвержден Не\ подходит Отказался)
 
-  after_update :check_event
-
-  def check_event
-    if event_id_changed? && !event_id_was.nil?
-      Event.find(event_id_was).destroy
-    end
-  end
+  # after_update :check_event
+  #
+  # def check_event
+  #   if event_id_changed? && !event_id_was.nil?
+  #     Event.find(event_id_was).destroy
+  #   end
+  # end
 
   def self.return_status(options)
     staff_relation = where(candidate_id: options[:vacancy][:candidate_id], vacancy_id: options[:id]).first
