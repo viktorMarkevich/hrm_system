d2524b042567d0fc25ce36277d5e536eb781607d
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-09-22 12:29:42 +0300

    try to fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :deploy_to, "/home/deployer/#{fetch(:stage)}/faceit-hrm"
 # set :rvm_ruby_version, 'ruby-2.2.2@faceit-hrm'      # Defaults to: 'default'
 set :rvm_type, :user                     # Defaults to: :auto
 set :rvm_ruby_version, '2.2.2'      # Defaults to: 'default'
+set :rvm_custom_path, "~#{user}/.rvm/bin/rvm gemset create #{application}"
 
 
 # Тип запуска Rails, метод доставки обновлений и локальные релизные версии
