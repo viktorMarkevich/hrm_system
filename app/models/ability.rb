2074fd24aa08005ec36b4ab9c33d4e3c2699a449
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-09-24 10:51:26 +0300

    fix ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,13 +7,14 @@ class Ability
     # alias_action :create, :read, :update, :destroy, to: :crud
 
     can :manage, Sticker, owner_id: user.id
-    can :manage, User, id: user.id
+    # can :manage, User, id: user.id
 
     if user.post == 'Директор'
-      can :read, [Vacancy, Company, Candidate]
+      can :read, [ Vacancy, Company, Candidate]
+      can :manage, Event, user_id: user.id
     elsif user.post == 'HR Менеджер'
-      can :destroy, [Vacancy, Company, Candidate], owner_id: user.id
-      can [:read, :create, :update], [Vacancy, Company, Candidate]
+      can [:destroy, :update], [Event, Vacancy, Company, Candidate], user_id: user.id
+      can [:read, :create], [Event, Vacancy, Company, Candidate]
     end || []
 
   end
