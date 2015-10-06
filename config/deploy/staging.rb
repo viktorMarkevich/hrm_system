dd366890983106cd25c39fc8b4fe9781fb484513
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-10-06 11:16:51 +0300

    chenge syntax

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,8 +12,6 @@ set :migration_role, 'migrator'
 # Defaults to [:web]
 set :assets_roles, [:web, :app]
 
-server '192.168.137.75', user: 'deployer', roles: %w{app db web}
-
 # role-based syntax
 # ==================
 
@@ -41,29 +39,13 @@ set :rvm_ruby_version, "2.2.2@#{fetch(:application)}"
 set :branch, 'develop'
 
 # Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-set :ssh_options, {
-   keys: %w(/home/deployer/.ssh/id_rsa),
-   forward_agent: true,
-   auth_methods: %w(publickey password),
-   password: ENV['STAGING_SSH_PASSWORD']
-}
-#
-# The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server '192.168.137.75',
+       user: 'deployer',
+       roles: %w{app db web},
+       ssh_options: {
+           keys: %w(/home/deployer/.ssh/id_rsa),
+           forward_agent: true,
+           auth_methods: %w(publickey password),
+           password: ENV['STAGING_SSH_PASSWORD']
+       }
