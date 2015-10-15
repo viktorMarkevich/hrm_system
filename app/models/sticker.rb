5b98b9f048ef2cfa8ed566e30df9ba65105f07d6
Viktor Markevich <viktor.markevich@faceit.com.ua>
2015-10-15 15:10:49 +0300

    fix error occurs after creating a sticker

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,10 +1,9 @@
 class Sticker < ActiveRecord::Base
   acts_as_paranoid
 
-  BG_COLOR = %w(#FF3300 #FF66FF #6666FF #00CCFF #00CC66 #009900
+  BG_COLOR = %w(#FF8533 #FF66FF #9797FF #00CCFF #00CC66 #009900
                 #99CC00 #FFFF66 #FF9933 #FF9999 #CC6699 #CCCC00
-                #FF3333 #3399FF #339966 #9900CC #D63385 #CC66FF)
-
+                #FF6666 #3399FF #339966 #9900CC #D63385 #CC66FF)
 
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
 
