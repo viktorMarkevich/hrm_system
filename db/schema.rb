36ae3661af6461ec26020662a89900a577c74369
Victor <viktor.markevich@faceit.com.ua>
2015-11-23 21:20:24 +0200

    add column to candidates; change markup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118125338) do
+ActiveRecord::Schema.define(version: 20151123190943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20151118125338) do
     t.integer  "user_id"
     t.integer  "company_id"
     t.string   "notice"
+    t.text     "experience"
   end
 
   create_table "companies", force: :cascade do |t|
