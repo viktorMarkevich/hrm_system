34949bae29bc14fcef411f8f3edcb834f0e8dffe
SkrypkoA <skrypko.aleksandr@gmail.com>
2017-03-02 14:20:46 +0200

    add geo_name and geo_alternate_name facroties

--- app/controllers/geo_names_controller.rb
+++ app/controllers/geo_names_controller.rb
@@ -4,10 +4,10 @@ class GeoNamesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    geo_alternate_name_list = GeoAlternateName.joins(:geo_name).where(language: 'ru', geo_names: {fclass: 'P'}).pluck(:name)
+    geo_alternate_name_list = GeoAlternateName.joins(:geo_name).where(language: 'ru', geo_names: { fclass: 'P' }).pluck(:name)
     respond_to do |format|
       format.html
-      format.json {render json: geo_alternate_name_list}
+      format.json { render json: geo_alternate_name_list }
     end
   end
 end
--- app/models/geo_name.rb
+++ app/models/geo_name.rb
@@ -1,5 +1,6 @@
 class GeoName < ApplicationRecord
   has_many :geo_alternate_names, foreign_key: 'geo_geoname_id'
   has_many :candidates
+  
   validates :name, uniqueness: true
 end
