1cbb08ba896dff4b8b6ab4e30fdc4691d715f8a6
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-03-02 12:02:45 +0200

    Refactor regexp for cv parsing

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -27,7 +27,7 @@ class Candidate < ActiveRecord::Base
   validates :phone, uniqueness: true, if: 'phone.present?'
   validates :skype, uniqueness: true,
                     length: { minimum: 6, maximum: 32 },
-                    format: { with: /(\A[a-zA-Z]+\w*([-.:]\w+)*\z)/, message: 'is invalid.' },
+                    format: { with: /(\A[a-zA-Z]+\w*(?:[-.:]\w+)*\z)/, message: 'is invalid.' },
                     if: 'skype.present?'
   #validates :birthday, format: { with: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/, multiline: true,
   #          message: 'wrong format' }, if: 'birthday.present?'
@@ -42,22 +42,22 @@ class Candidate < ActiveRecord::Base
 
   def save_resume_to_candidate(data)
     content = Yomu.new(data).text.to_s
-    self.name = content.scan(/([A-Z]+[a-zA-Z]* [A-Z]+[a-zA-Z]*)|([А-Я]+[а-яА-Я]* [А-Я]+[а-яА-Я]*)/).first.compact.first.strip
+    self.name = content.scan(/(?:[A-Z]+[a-zA-Z]* [A-Z]+[a-zA-Z]*)|(?:[А-Я]+[а-яА-Я]* [А-Я]+[а-яА-Я]*)/).compact.first.strip
     self.birthday = content.scan(/\d{1,2}\-\d{1,2}\-\d{2,4}/).first.strip ||
         content.scan(/\d{1,2}\/\d{1,2}\/\d{2,4}/).first.strip ||
         content.scan(/\d{1,2}\.\d{1,2}\.\d{2,4}/).first.strip
-    self.salary = content.scan(/([^\s][-~]*[0-9]{2,7}\s*)(?=грн|ГРН|usd|USD|долл|\$)/).first.strip
-    self.city_of_residence = content.scan(/(?<=Город:|Регион:|Адрес:)[^\n\r]*/).first.strip
+    self.salary = content.scan(/^*\s*(?=[-~])*[0-9]{2,7}\s*(?=грн|ГРН|usd|USD|долл|\$)/).first.strip
+    self.city_of_residence = content.scan(/(?<=Город:|Регион:|Адрес:)\s*$*.*(?=$)/).first.strip
     self.source = data.original_filename
     # self.education = content.scan(/(?<=[Оо]бразование[:\n]|[Ee]ducation[:\n])(.|\n)*\w+/).join('')
-    self.languages = content.scan(/(?:[\s]|^)(English|Английский|Англійська|Russian|Русский|Російська|Ukrainian|Украинский|Українська|Français|Polish)(?:[.,:;-]*)(?=[\s]|$)/).compact.join(', ')
+    self.languages = content.scan(/^*\s*(?:English|Английский|Англійська|Russian|Русский|Російська|Ukrainian|Украинский|Українська|Français|Polish)(?=[\.\-,:;\s$])/).compact.join(', ')
     self.email = content.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i).first.strip
     self.phone = content.scan(/\b((?:[\s()\d-]{11,}\d)|\d{10,})\b/).join(', ')
-    self.skype = content.scan(/(?<=[Ss]kype:[\s\n])\s*\n*\w*(?=[\s]|$)/).compact.first.strip
-    self.linkedin = content.scan(/(?<=[Ll]inked[Ii]n:[\s\n]).*(?=[\s]|$)/).first.strip
-    self.facebook = content.scan(/(?<=[Ff]acebook:[\s\n]).*(?=[\s]|$)/).first.strip
-    self.vkontakte = content.scan(/(?<=[Vv]kontakte:[\s\n]|[Vv][Kk]:[\s\n]).*(?=[\s]|$)/).first.strip
-    self.google_plus = content.scan(/(?<=[Gg]oogle\+:[\s\n]|[Gg]oogle[Pp]lus:[\s\n]).*(?=[\s]|$)/).first.strip
+    self.skype = content.scan(/(?<=[Ss]kype:)\s*[a-zA-Z]+\w*(?:[-.:]\w+)*(?=[\s$])/).compact.first.strip
+    self.linkedin = content.scan(/(?<=[Ll]inked[Ii]n:)\s*.*(?=[\s$])/).first.strip
+    self.facebook = content.scan(/(?<=[Ff]acebook:)\s*.*(?=[\s$])/).first.strip
+    self.vkontakte = content.scan(/(?<=[Vv]kontakte:|[Vv][Kk]:)\s*.*(?=[\s$])/).first.strip
+    self.google_plus = content.scan(/(?<=[Gg]oogle\+:|[Gg]oogle[Pp]lus:)\s*.*(?=[\s$])/).first.strip
     self.description = content
 
     # self.experience = content.scan(/(?<=Опыт работы\n)((.|\n)*?)(?=Образование\n)/).join('')
