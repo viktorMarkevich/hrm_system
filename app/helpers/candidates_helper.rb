b2e67e45e6a12e92f539c2decfe2d0d8f67ba42d
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-03-10 11:57:05 +0200

    Get back labels to vacancies list on candidate page

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -20,6 +20,25 @@ module CandidatesHelper
     ((100.0*current_attr_numbs)/attr_numbs).round(1)
   end
 
+  def get_sr_label_class(status)
+    case status
+      when 'Найденные'
+        'label-default'
+      when 'Отобранные'
+        'label-primary'
+      when 'Собеседование'
+        'label-info'
+      when 'Утвержден'
+        'label-success'
+      when 'Не подходит'
+        'label-warning'
+      when 'Отказался'
+        'label-danger'
+      else
+        []
+    end
+  end
+
   def sr_status(vacancy, candidate)
     vacancy.staff_relations.select{|s| s.candidate_id == candidate.id }.first.status
   end
