72ed9367a50ebb44e8f9e9e65796e00decf73713
Boychenko Nataliya <natalyasvd@gmail.com>
2017-04-18 15:37:18 +0300

    Add test for Rufus

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,6 +16,28 @@ class Event < ActiveRecord::Base
   def self.events_of(user, from, to)
     user.events.where(will_begin_at: from..to).order(will_begin_at: :asc)
   end
+  def self.events_soon_mailer
+    @events_soon = where(will_begin_at: (Time.zone.now+1.day).at_beginning_of_day()..(Time.zone.now+1.day).at_end_of_day())
+    p @events_soon
+    p "@events_soon"*10
+    if @events_soon.count > 0
+      @events_soon.each do |event|
+        if  event.staff_relation && event.staff_relation.candidate && event.staff_relation.candidate.owner
+          owner = event.staff_relation.candidate.owner
+          NoticeMailer.event_soon(event, owner).deliver_now
+        end
+        if  event.staff_relation && event.staff_relation.candidate && event.staff_relation.candidate.owner
+          candidate = event.staff_relation.candidate
+
+          NoticeMailer.event_soon_candidate(event, candidate).deliver_now
+        end
+        if  event.staff_relation && event.staff_relation.candidate && event.staff_relation.candidate.owner && User.where(post: 'Директор').first
+          ceo = User.where(post: 'Директор').first
+          NoticeMailer.event_soon(event, ceo).deliver_now
+        end
+      end
+    end
+  end
 
   def start_time
     self.will_begin_at
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,23 +1,8 @@
 require 'rufus-scheduler'
-s = Rufus::Scheduler.singleton
-s.every '1d' do
+s = Rufus::Scheduler.new
+s.every '10s' do
   Rails.logger.info "Helo #{Time.now}"
-  @events_soon = Event.where(will_begin_at: (Time.zone.now+1.day).at_beginning_of_day()..(Time.zone.now+1.day).at_end_of_day())
-  if @events_soon.count > 0
-    @events_soon.each do |event|
-      if  event.staff_relation && event.staff_relation.candidate && event.staff_relation.candidate.owner
-        owner = event.staff_relation.candidate.owner
-        NoticeMailer.event_soon(event, owner).deliver_now
-      end
-      if  event.staff_relation && event.staff_relation.candidate && event.staff_relation.candidate.owner
-        candidate = event.staff_relation.candidate
 
-        NoticeMailer.event_soon_candidate(event, candidate).deliver_now
-      end
-      if  event.staff_relation && event.staff_relation.candidate && event.staff_relation.candidate.owner && User.where(post: 'Директор').first
-        ceo = User.where(post: 'Директор').first
-        NoticeMailer.event_soon(event, ceo).deliver_now
-      end
-    end
-  end
+  Event.events_soon_mailer
+
 end
