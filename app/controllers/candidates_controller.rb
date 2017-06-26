4b3215babc44c7ab49fcdc7f9981c6d7f55409c2
Igor <igor.polyakov@faceit-team.com>
2017-06-26 11:05:07 +0300

    fix n+1 error

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -8,7 +8,7 @@ class CandidatesController < ApplicationController
   def index
     @status = params[:status]
     if params[:tags]
-      @candidates = Candidate.tagged_with(params[:tags], any: true).where('name ILIKE ?', "%#{params[:term]}%").page(params[:page]).per(10)
+      @candidates = Candidate.preload(:owner).tagged_with(params[:tags], any: true).where('name ILIKE ?', "%#{params[:term]}%").page(params[:page]).per(10)
     else
       @candidates = Candidate.preload(:owner).where(filter_condition).order('id').page(params[:page]).per(10)
     end
