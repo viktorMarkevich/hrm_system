a04fa0167638bfc9f62c4eca212549ac88e03f3b
Victor <viktor.markevich@faceit.com.ua>
2017-08-14 17:13:37 +0300

    Remove procfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@bitbucket.org:hrm_system_team/faceit-hrm.git'
 
 set :rvm_type, :user                     # Defaults to: :auto
 set :rvm_ruby_version, -> { "2.4.0@#{fetch(:application)} --create" }
+# set :rvm_ruby_version, -> { "2.4.0@#{fetch(:application)}_#{fetch(:rails_env)} --create" }
 
 # Default value for :format is :pretty
 set :format, :pretty
