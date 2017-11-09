e34d9ebc7053b52bc3b7851b2a4b6b95bb6c0c73
Victor <viktor.markevich@faceit.com.ua>
2017-11-09 16:25:43 +0200

    Update cap  version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.9.0'
+lock '3.10.0'
 
 set :application, 'faceit-hrm'
 set :repo_url, 'git@bitbucket.org:hrm_system_team/faceit-hrm.git'
