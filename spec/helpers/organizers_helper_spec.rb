af1380eb304cac6e17e2deed6b7a707869cdc743
Victor <viktor.markevich@faceit.com.ua>
2019-05-20 16:45:36 +0300

    Cover organizers_helper

--- spec/helpers/organizers_helper_spec.rb
+++ spec/helpers/organizers_helper_spec.rb
@@ -9,29 +9,29 @@ RSpec.describe OrganizersHelper, type: :helper do
 
   let(:history) { create :history, historyable_type: 'Vacancy', historyable_id: vacancy.id }
 
-  let(:history_sr) { create :history, historyable_type: 'StaffRelation', historyable_id: staff_relation.id  }
+  let(:history_sr) { create :history, historyable_type: 'StaffRelation', historyable_id: staff_relation.id, was_changed: { "salary"=>"[\"550\", \"100\"]" } }
 
   context 'methods helper' do
     it 'method set_owner_for_historyable' do
-      expect(helper.set_owner_for_historyable(history).html_safe).to eq("<a href=\"/users/#{vacancy.owner.id}\">#{vacancy.owner.full_name}</a>")
+      expect(helper.set_owner_for_historyable(history)).to eq("<a href=\"/users/#{vacancy.owner.id}\">#{vacancy.owner.full_name}</a>")
     end
 
     context 'set_action_for' do
       it 'method set_action_for VACATION' do
         #temp
-        expect(helper.set_action_for(history).html_safe).to eq("<span class=\"translation_missing\" title=\"translation missing: ru.history.vacancy.notice, object_name: &lt;a href=&quot;http://test.host/vacancies/#{vacancy.id}&quot;&gt;Вакансия_#{vacancy.id}&lt;/a&gt;\">Notice</span>")
+        expect(helper.set_action_for(history)).to eq("<span class=\"translation_missing\" title=\"translation missing: ru.history.vacancy.notice, object_name: &lt;a href=&quot;http://test.host/vacancies/#{vacancy.id}&quot;&gt;Вакансия_#{vacancy.id}&lt;/a&gt;\">Notice</span>")
       end
 
       it 'method set_action_for STAFF_RELATION' do
         #temp
-        expect(helper.set_action_for(history_sr).html_safe).to eq("<span class=\"translation_missing\" title=\"translation missing: ru.history.staff_relation.notice, vacancy_name: &lt;a href=&quot;/vacancies/#{history_sr.historyable.vacancy.id}&quot;&gt;Вакансия_#{history_sr.historyable.vacancy.id}&lt;/a&gt;, candidate_name: &lt;a href=&quot;/candidates/#{history_sr.historyable.candidate.id}&quot;&gt;#{history_sr.historyable.candidate.name}&lt;/a&gt;\">Notice</span>")
+        expect(helper.set_action_for(history_sr)).to eq("<span class=\"translation_missing\" title=\"translation missing: ru.history.staff_relation.notice, vacancy_name: &lt;a href=&quot;/vacancies/#{history_sr.historyable.vacancy.id}&quot;&gt;Вакансия_#{history_sr.historyable.vacancy.id}&lt;/a&gt;, candidate_name: &lt;a href=&quot;/candidates/#{history_sr.historyable.candidate.id}&quot;&gt;#{history_sr.historyable.candidate.name}&lt;/a&gt;\">Notice</span>")
       end
     end
 
-    # context 'set_status_for' do
-    #   it 'method set_status_for' do
-    #     expect(helper.set_status_for(history).html_safe).to eq("")
-    #   end
-    # end
+    context 'set_status_for' do
+      it 'method set_status_for' do
+        expect(helper.set_status_for(history_sr)).to eq("<span class=\"translation_missing\" title=\"translation missing: ru.activerecord.attributes.staff_relation.salary\">Salary</span><span class=\"translation_missing\" title=\"translation missing: ru.history.staff_relation.changes, val_from: 550, val_to: 100\">Changes</span>")
+      end
+    end
   end
 end
